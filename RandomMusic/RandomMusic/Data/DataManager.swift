8bd14b0d5f5aff3eabdc9cff76dc546af4fb1eb2
kanghun1121 <kanghun1121@naver.com>
2025-06-09 11:12:33 +0900

    feat: DataManager 초기화 작업 구성

--- RandomMusic/RandomMusic/Data/DataManager.swift
+++ RandomMusic/RandomMusic/Data/DataManager.swift
@@ -4,25 +4,44 @@ import CoreData
 final class DataManager {
     static let shared = DataManager()
 
-    private init() {}
+    let mainContext: NSManagedObjectContext
+    let persistentContainer: NSPersistentContainer
 
-    var mainContext: NSManagedObjectContext {
-        return persistentContainer.viewContext
-    }
+    let fetchedResults: NSFetchedResultsController<Song>
 
-    // MARK: - Core Data stack
-    lazy var persistentContainer: NSPersistentContainer = {
+    private init() {
         let container = NSPersistentContainer(name: "RandomMusic")
 
-        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+        container.loadPersistentStores { storeDescription, error in
             if let error = error as NSError? {
                 fatalError("Unresolved error \(error), \(error.userInfo)")
             }
-        })
-        return container
-    }()
+        }
 
-    // MARK: - Core Data Saving support
+        persistentContainer = container
+        mainContext = container.viewContext
+
+        let request = Song.fetchRequest()
+        let sortedByDate = NSSortDescriptor(keyPath: \Song.insertDate, ascending: false)
+        request.sortDescriptors = [sortedByDate]
+
+        fetchedResults = NSFetchedResultsController(
+            fetchRequest: request,
+            managedObjectContext: mainContext,
+            sectionNameKeyPath: nil,
+            cacheName: nil
+        )
+
+        do {
+            try fetchedResults.performFetch()
+        } catch {
+            print(error)
+        }
+    }
+
+	// TODO: Insert 정의
+
+    /// Context에 저장된 데이터를 영구적으로 저장합니다.
     func saveContext() {
         let context = persistentContainer.viewContext
 
