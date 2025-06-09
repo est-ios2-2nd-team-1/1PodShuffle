02e6e4e86f1555b71c12cb98a4932db009a78f87
김종성 <jseongee@gmail.com>
2025-06-09 10:38:37 +0900

    ui: 메인화면 와이어프레임 추가

--- RandomMusic/RandomMusic/View/ViewController/MainViewController.swift
+++ RandomMusic/RandomMusic/View/ViewController/MainViewController.swift
@@ -1,9 +1,13 @@
 import UIKit
 
 class MainViewController: UIViewController {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var singerLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        titleLabel.text = "Willow"
+        singerLabel.text = "Taylor Swift"
     }
 }
-
