b0a7ec2ea1a75b6095fe6d10c1e30249d855f7d0
KangKang <165460077+kanghun1121@users.noreply.github.com>
2025-06-09 15:58:13 +0900

    feat: DataManager 추가 (#3)
    * feat: DataManager 초기화 작업 구성
    
    * feat: 임시 DTO 추가 및 Insert 메소드 추가
    
    * feat: delete 메소드 추가 및 이미지 데이터 저장 및 삭제 로직 추가
    
    * feat: DataManager 장르 추천 메소드 구현
    
    * refactor: 장르 메소드 로직 리팩토링
    
    * chore: Entity 파일 분리

---
+++ RandomMusic/RandomMusic/Model/SongEntity.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+struct SongEntity {
+    let title: String
+    let album: String
+    let artist: String
+    let genre: String
+    let id: Int
+    let streamUrl: String
+    let thumbnail: Data?
+}
