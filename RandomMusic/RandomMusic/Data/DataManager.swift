e10049cce4df8f858001f3e3d39790f2e9a4251a
kanghun1121 <kanghun1121@naver.com>
2025-06-09 12:57:02 +0900

    feat: 임시 DTO 추가 및 Insert 메소드 추가

--- RandomMusic/RandomMusic/Data/DataManager.swift
+++ RandomMusic/RandomMusic/Data/DataManager.swift
@@ -1,6 +1,16 @@
 import Foundation
 import CoreData
 
+struct SongDTO {
+    let title: String
+    let album: String
+    let artist: String
+    let genre: String
+    let id: Int
+    let streamUrl: String
+    let thumbnail: Data?
+}
+
 final class DataManager {
     static let shared = DataManager()
 
@@ -38,10 +48,33 @@ final class DataManager {
             print(error)
         }
     }
+    
+    /// 음악 데이터를 영구적으로 저장합니다.
+    /// - Parameter song: 저장할 음악 데이터를 받습니다.
+    func insertSongData(from song: SongDTO) {
+        let newSong = Song(context: mainContext)
+        newSong.title = song.title
+        newSong.album = song.album
+        newSong.artist = song.artist
+        newSong.genre = song.genre
+        newSong.id = Int64(song.id)
+        newSong.streamUrl = song.streamUrl
+        newSong.insertDate = .now
 
-	// TODO: Insert 정의
+        // 이미지 데이터는 용량이 크기 때문에 디렉토리에 따로 저장해두고 해당 디렉토리를 가르키는 URL만 따로 CoreData에 저장합니다.
+        let thumbnailFileName = String(newSong.id) + ".png"
+        if let url = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent(thumbnailFileName) {
+            do {
+                try song.thumbnail?.write(to: url)
+                newSong.thumbnail = thumbnailFileName
+                saveContext()
+            } catch {
+                print(error)
+            }
+        }
+    }
 
-    /// Context에 저장된 데이터를 영구적으로 저장합니다.
+    /// Context에 임시 저장된 데이터를 영구적으로 저장합니다.
     func saveContext() {
         let context = persistentContainer.viewContext
 
