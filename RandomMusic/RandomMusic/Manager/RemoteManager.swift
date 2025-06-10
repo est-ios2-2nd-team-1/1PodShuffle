b4c2674d9e89d3099ecfdcff33897e0c130c3ba7
kanghun1121 <kanghun1121@naver.com>
2025-06-10 17:36:00 +0900

    feat: MPRemoteCommendCenter 메소드 구현

--- RandomMusic/RandomMusic/Manager/RemoteManager.swift
+++ RandomMusic/RandomMusic/Manager/RemoteManager.swift
@@ -12,28 +12,44 @@ final class RemoteManager {
     }
 
     func configure() {
+        try? AVAudioSession.sharedInstance().setCategory(.playback, mode: .default, options: [])
+        try? AVAudioSession.sharedInstance().setActive(true)
+
+        commandCenter.playCommand.isEnabled = true
         commandCenter.playCommand.addTarget { [weak self] _ in
             // TODO: PlayerManager에게 재생을 요청해야 한다 어떻게?
             // self?.playerManager?.play()
             return .success
         }
 
+        commandCenter.pauseCommand.isEnabled = true
         commandCenter.pauseCommand.addTarget { [weak self] _ in
             // TODO: PlayManager에 퍼즈를 요청해야 한다.
             self?.playerManager?.pause()
             return .success
         }
 
+        commandCenter.nextTrackCommand.isEnabled = true
         commandCenter.nextTrackCommand.addTarget { _ in
             // TODO: PlayManager에 이전 곡을 요청해야 한다.
             //self?.playerManager?.before()
             return .success
         }
 
+        commandCenter.previousTrackCommand.isEnabled = true
         commandCenter.previousTrackCommand.addTarget { _ in
 			// TODO: PlayManager에 다음 곡을 요청해야 한다.
             // self?.playerManager?.next()
             return .success
         }
+
+        var nowPlayingInfo: [String: Any] = [:]
+        nowPlayingInfo[MPMediaItemPropertyTitle] = "Hello" // 곡 제목
+        nowPlayingInfo[MPMediaItemPropertyArtist] = "Muse" // 아티스트 이름
+        //nowPlayingInfo[MPMediaItemPropertyPlaybackDuration] = -> 총 재생 시간 (초)
+        //nowPlayingInfo[MPNowPlayingInfoPropertyElapsedPlaybackTime] = -> 현재 재생 위치 (초)
+        nowPlayingInfo[MPNowPlayingInfoPropertyPlaybackRate] = 1.0 // 재생 속도
+
+        MPNowPlayingInfoCenter.default().nowPlayingInfo = nowPlayingInfo
     }
 }
