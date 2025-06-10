df7a46b144c8e8b8e368d9f1341c7af8884548ed
유정 <llyujay95@gmail.com>
2025-06-10 09:46:18 +0900

    ui: 장르 버튼 추가 / style: 코드 추가

--- RandomMusic/RandomMusic/View/ViewController/OnBoarding/GenreCell.swift
+++ RandomMusic/RandomMusic/View/ViewController/OnBoarding/GenreCell.swift
@@ -8,5 +8,21 @@
 import UIKit
 
 class GenreCell: UICollectionViewCell {
-    
+
+    @IBOutlet weak var genreButton: UIButton!
+    @IBOutlet weak var genreLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        genreButton.layer.cornerRadius = genreButton.bounds.height / 2
+        genreButton.clipsToBounds = true
+    }
+
+    func configure(with genre: Genre, selected: Bool) {
+        genreButton.setImage(UIImage(named: genre.iconName), for: .normal)
+        genreLabel.text = genre.name
+        genreButton.isSelected = selected
+        genreButton.setImage(UIImage(systemName: selected ? "checkmark.circle.fill" : "circle"), for: .selected)
+    }
+
 }
