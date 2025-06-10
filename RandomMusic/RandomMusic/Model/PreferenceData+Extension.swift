5217c51cf6e98c2555f93d3ff20a90742d52c163
drfranken <daranic9@gmail.com>
2025-06-10 16:51:42 +0900

    feat: 선호도 관련 기능 추가
    - 앱 설치후 최초실행,
    - 선호 장르 선택,
    - 각 곡마다 좋아요 싫어요
    - 좋아요 싫어요 취소
    할 떄마다 선호도 데이터가 쌓이거나 변경되도록 작성

---
+++ RandomMusic/RandomMusic/Model/PreferenceData+Extension.swift
@@ -0,0 +1,20 @@
+//
+//  PreferenceData+Extension.swift
+//  RandomMusic
+//
+//  Created by drfranken on 6/10/25.
+//
+
+import CoreData
+import Foundation
+
+extension PreferenceData {
+    convenience init(context: NSManagedObjectContext, genre: Genre, score: Double, isImmutable: Bool = false) {
+        self.init(context: context)
+        self.id = UUID()
+        self.genre = genre.rawValue
+        self.score = score
+        self.insertDate = Date()
+        self.isImmutable = isImmutable
+    }
+}
