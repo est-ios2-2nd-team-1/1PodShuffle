81a7b7ca0d5e70d08d9431505884e36bea324712
유정 <llyujay95@gmail.com>
2025-06-12 11:06:24 +0900

    feat: setting에 초기화 기능 추가 / ui: 아이패드가로모드 관련 수정

--- RandomMusic/RandomMusic/View/ViewController/Setting/SettingViewController.swift
+++ RandomMusic/RandomMusic/View/ViewController/Setting/SettingViewController.swift
@@ -2,7 +2,38 @@ import UIKit
 
 class SettingViewController: UIViewController {
 
+    private let preferenceManager = PreferenceManager()
+
+    private let resetButton = UIButton(type: .system)
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupUI()
     }
+
+    private func setupUI() {
+
+        resetButton.setTitle("추천 알고리즘 초기화ㅏ", for: .normal)
+        resetButton.backgroundColor = UIColor.systemBackground
+    }
+
+
+    @objc private func resetButtonTapped() {
+        let alert = UIAlertController(
+            title: "추천 알고리즘 초기화",
+            message: "데이터가 모두 삭제됩니다. 계속하시겠습니까?",
+            preferredStyle: .alert
+        )
+
+        alert.addAction(UIAlertAction(title: "취소", style: .cancel, handler: nil))
+//        alert.addAction(UIAlertAction(title: "초기화", style: .destructive, handler: { _ in
+//            self.preferenceManager.resetPreferences()
+//        }))
+
+        present(alert, animated: true, completion: nil)
+
+    }
+
 }
+
+
