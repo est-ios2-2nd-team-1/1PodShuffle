68aaf62ee53540ba04c15992ddd469130d83b0fa
kanghun1121 <kanghun1121@naver.com>
2025-06-13 17:11:12 +0900

    feat: 다음 곡 이전 곡 쓰로틀 적용

---
+++ RandomMusic/RandomMusic/Util/Throttle.swift
@@ -0,0 +1,21 @@
+import Foundation
+
+
+final class Throttle {
+    var isBlocking: Bool = false
+
+    /// Throttle을 적용합니다.
+    /// - Parameters:
+    ///   - block: 적용할 실행 구문을 작성합니다.
+    ///   - seconds: 소수점 형태로 몇 초간 Throttle 할 건지 입력합니다. (기본 값 0.3)
+    func run(_ block: @escaping () -> Void, seconds: Double = 0.3) {
+        guard !isBlocking else { return }
+        isBlocking = true
+        block()
+
+        Task {
+            try await Task.sleep(for: .seconds(seconds))
+            isBlocking = false
+        }
+    }
+}
