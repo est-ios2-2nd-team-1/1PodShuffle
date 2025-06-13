a3ed7b9abc89d11e443fac342195c7f6aa1af252
KangKang <165460077+kanghun1121@users.noreply.github.com>
2025-06-13 17:21:30 +0900

    refactor: 쓰로틀 파라미터 순서 변경 (#67)

--- RandomMusic/RandomMusic/Util/Throttle.swift
+++ RandomMusic/RandomMusic/Util/Throttle.swift
@@ -1,6 +1,5 @@
 import Foundation
 
-
 final class Throttle {
     private var isBlocking: Bool = false
 
@@ -8,7 +7,7 @@ final class Throttle {
     /// - Parameters:
     ///   - block: 적용할 실행 구문을 작성합니다.
     ///   - seconds: 소수점 형태로 몇 초간 Throttle 할 건지 입력합니다. (기본 값 0.3)
-    func run(_ block: @escaping () -> Void, seconds: Double = 0.3) {
+    func run(seconds: Double = 0.3, _ block: @escaping () -> Void) {
         guard !isBlocking else { return }
         isBlocking = true
         block()
